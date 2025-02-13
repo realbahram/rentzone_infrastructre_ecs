7cfc637556b6bdc34ac8870d13520d398b6a7398
bahram demo <bahramunited@gmail.com>
2025-02-13 20:22:16 +1100

    created security groups

--- terrafrom/variables.tf
+++ terrafrom/variables.tf
@@ -49,3 +49,9 @@ variable "private_data_subnet_az2_cidr" {
   description = "private data subnet az2 cidr block"
   type        = string
 }
+
+# Security-Groups Variables
+variable "ssh_location" {
+  description = "IP addressed used for ssh into server"
+  type        = string
+}
