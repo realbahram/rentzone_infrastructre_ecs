6b8ab45f9a69bd108a81353671cf20eef9466232
bahram demo <bahramunited@gmail.com>
2025-02-19 18:04:00 +1100

    changing backend

--- terrafrom/backend.tf
+++ terrafrom/backend.tf
@@ -1,10 +1,10 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = "terraform-remotestate-bahram"
+    bucket         = "terraform-state-aws-learner"
     key            = "rentzone/terraform.tfstate"
     region         = "us-east-1"
-    profile        = "bahram"
+    profile        = "aws_learner"
     dynamodb_table = "terraform-state-lock"
   }
 }
--- terrafrom/providers.tf
+++ terrafrom/providers.tf
@@ -1,7 +1,7 @@
 # configure aws provider to establish a secure connection between terraform and aws
 provider "aws" {
   region = var.region
-  profile = "bahram"
+  profile = "aws_learner"
 
   default_tags {
     tags = {
